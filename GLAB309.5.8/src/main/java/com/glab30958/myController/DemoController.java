f772cba247151173780263549ecca0710b823700
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-08-14 21:57:34 -0400

    GLAB to practice Spring Boot Actuator

---
+++ GLAB309.5.8/src/main/java/com/glab30958/myController/DemoController.java
@@ -0,0 +1,12 @@
+package com.glab30958.myController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class DemoController {
+    @GetMapping("/get-info")
+    public String getStringMessage() {
+        return "Hi ! You will be able to monitor the health of the application !!" ;
+    }
+}
+
